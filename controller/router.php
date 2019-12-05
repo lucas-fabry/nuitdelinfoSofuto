e079663678746b944c008c99be2ba0d78c7b2416
ange-clement <47100137+ange-clement@users.noreply.github.com>
2019-12-05 19:19:53 +0100

    router et lib

---
+++ controller/router.php
@@ -0,0 +1,59 @@
+<?php
+require_once File::build_path(array('controleur','ControleurXXXX.php'));
+
+
+function myGet($nomVar) {
+    if (isset($_POST[$nomVar])){
+        return $_POST[$nomVar];
+    }
+    else if (isset($_GET[$nomVar])) {
+        return $_GET[$nomVar];
+    }
+    else {
+        return NULL;
+    }
+}
+
+function myCookie($nomVar) {
+    if (isset($_COOKIE[$nomVar])) {
+        return $_COOKIE[$nomVar];
+    }
+    else {
+        return null;
+    }
+}
+
+// On recupère l'action passée dans l'URL
+if (!is_null(myget("action"))){
+    $action = myget("action");
+}
+else {
+    $action = "readAll";
+}
+
+// On recupère le controller passée dans l'URL
+if (!is_null(myget("controller"))) {
+    $controller = myget("controller");
+}
+else {
+    $controller = "topic";
+}
+
+$controller_class = "controller".ucfirst($controller);
+
+if (class_exists($controller_class)) {
+    if (in_array($action, get_class_methods($controller_class))) {
+        $controller_class::$action();
+    }
+    else {
+        $vue = 'error';
+        $pagetitle = 'Erreur de methode';
+        require File::build_path(array('vue', 'vue.php'));
+    }
+}
+else {
+    $vue = 'error';
+    $pagetitle = 'Erreur, controleur non valide';
+    require File::build_path(array('vue', 'vue.php'));
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-include 'view/menu.php';
+session_start();
+$DS = DIRECTORY_SEPARATOR;
+require_once 'lib'.$DS.'Session.php';
+require_once 'lib'.$DS.'File.php';
+require 'controller'.$DS.'router.php';
 ?>
