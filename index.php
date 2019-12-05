1448e4409ba0a1cb47ec916398691a708b1f817d
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-05 19:47:08 +0100

    je deviens meilleur que Ange avec ce simple commit

--- index.php
+++ index.php
@@ -8,15 +8,3 @@ require 'controller'.$DS.'router.php';
 
 
 ?>
-
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-    <link rel="icon" href="images/index.ico" />
-</head>
-
-<?php
-require_once 'menu.php';
-?>
---
+++ view/vue.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+    <link rel="icon" href="images/index.ico" />
+    
+    <body class="amber lighten-5">
+        
+        <header>
+            <?php require_once 'menu.php'; ?>
+        </header>
+
+    </head>    
+        
+      
+    </body>
+</html>
