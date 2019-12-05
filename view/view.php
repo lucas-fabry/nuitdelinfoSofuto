1f3f40323ac2779bfc3302327e0ed7a45f595659
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-05 21:15:30 +0100

    je suis un petit génie de l'informatique

--- view/view.php
+++ view/view.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
