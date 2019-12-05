1099b2f31bdd2e0674646c5e0b5526c686b0a992
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-06 00:09:42 +0100

    petite correction d'un bug

--- view/view.php
+++ view/view.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <title><?php echo $pagetitle; ?></title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-        <link rel="stylesheet" href="css/styles.css">
+        
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         
         <link rel="icon" href="images/index.ico" />
