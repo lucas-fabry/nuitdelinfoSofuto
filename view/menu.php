cc172310da5fecaa2e8387b5d15468406f1c547f
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-05 18:50:48 +0100

    le menu est INCROYABLE

--- view/menu.php
+++ view/menu.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 </head>
 
 <body>
@@ -12,24 +12,24 @@
     <div class="nav-wrapper blue-grey">
 
 
-      <a href="#!" class="brand-logo" style="text-decoration:none">Aide O'etudiant</a>
+      <a href="#!" class="brand-logo" style="text-decoration:none; margin-left: 1vw;">Aide O'etudiant</a>
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-      <li><a href="signup.php" class="btn waves-effect waves-light deep-orange lighten-2">S'inscrire</a></li>
-      <li><a href="login.php" class="btn waves-effect waves-light deep-orange lighten-2">Se connecter</a></li>
-      <li><a href="contactUs.php" class="btn waves-effect waves-light deep-orange lighten-2" >Forum</a></li>
+      <li><a href="#?" class="btn waves-effect waves-light deep-orange lighten-2">S'inscrire</a></li>
+      <li><a href="#?" class="btn waves-effect waves-light deep-orange lighten-2">Se connecter</a></li>
+      <li><a href="#?" class="btn waves-effect waves-light deep-orange lighten-2" >Forum</a></li>
 
           </ul>
 
 
-       <!--<form class=" hide-on-med-and-down" id="formulaire" >
+       <form class="hide-on-med-and-down"  id="formulaire" style="margin-left: 20vw">
         <div class="input-field" style="max-width: 400pt;">
-          <input id="search" type="search" required onkeyup="getBlogs(this.value,0);">
+          <input id="search" type="search" required >
           <label class="label-icon" for="search"><i class="material-icons">search</i></label>
           <i class="material-icons">close</i>
           <div id="searchResults" ></div>
         </div>
-      </form>-->
+      </form>
 
 
        <ul class="side-nav" id="mobile-demo">
@@ -43,9 +43,11 @@
           <div id="searchResults2"></div>
         </div></li>
        </ul>
+
+
       </div>
       </nav>
       </div>
 
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+    
 </body>  
