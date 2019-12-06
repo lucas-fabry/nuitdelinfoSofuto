d381474614096d94af521d0d20551c23d08ed6e3
ange-clement <47100137+ange-clement@users.noreply.github.com>
2019-12-06 01:24:38 +0100

    joli

--- view/topic/list.php
+++ view/topic/list.php
@@ -1,13 +1,30 @@
 <div class="card-panel">
 	<a href="index.php?action=create&controller=topic"><button class="btn waves-effect waves-light" type="button"><i class="material-icons">add</i></button></a>
 </div>
+<div class="container">
 <?php
 foreach ($tab_t as $t) {
-	echo '<div class="card"><div class="card-content toExpand">';
-		echo '<div>'.htmlspecialchars($t->get("dateTopic")).'</div>';
-		echo '<div>'.htmlspecialchars($t->get("loginCompte")).'</div>';
-		echo '<div class="card-title">'.htmlspecialchars($t->get("nomTopic")).'</div>';
-		echo '<button class="expandButton btn waves-effect waves-light"><i class="material-icons">expand_more</i></button>';   
-		echo '<p>'.htmlspecialchars($t->get('texteTopic')).'</p>';
-    echo '</div></div>';
+	echo '<div class="card"><div class="card-content toExpand">'."\n";
+		echo '<div>'.htmlspecialchars($t->get("dateTopic")).'</div>'."\n";
+		echo '<div>'.htmlspecialchars($t->get("loginCompte")).'</div>'."\n";
+		echo '<div class="card-title">'.htmlspecialchars($t->get("nomTopic")).'</div>'."\n";
+		echo '<p>'.htmlspecialchars($t->get('texteTopic')).'</p>'."\n";
+
+		if (Session::is_user($t->get('loginCompte'))) {
+			echo '<div class="card-action">';
+		    echo '<a href="index.php?action=update&controller=topic&'
+		            . 'idTopic='.rawurlencode($t->get("idTopic")).'">'
+                    . 'Modifier le topic'
+	            . '</a>';
+		    echo '<a href="index.php?action=delete&controller=topic&'
+		            . 'idTopic='.rawurlencode($t->get("idTopic")).'">'
+		            . 'Supprimer le topic'
+	            . '</a>'."\n";
+	        echo '</div>';
+		}
+		echo '<button class="expandButton btn waves-effect waves-light"><i class="material-icons">expand_more</i></button>'."\n";   
+	
+    echo '</div></div>'."\n"."\n";
 }
+?>
+</div>
