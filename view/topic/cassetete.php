621b484cfbc52c839034d2e6459f655ebcb55d9b
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-06 01:43:27 +0100

    La y'a vrmt un truc impressionnat

---
+++ view/topic/cassetete.php
@@ -0,0 +1,5 @@
+<img src="https://image.aveshack.com/8ce9abc24ddc6457.png"/> <h1>Coffre n°1</h1><p>J'ai la clé</p>
+
+<img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <h1>Coffre n°2</h1><p>Je n'ai pas la clé</p>
+
+<img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <h1>Coffre n°3</h1><p>Le premier n'a pas la clé</p>
