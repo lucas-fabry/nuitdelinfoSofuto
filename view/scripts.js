bdded4ec5ae8846b698c879e2d2f21d038c0c7a3
ange-clement <47100137+ange-clement@users.noreply.github.com>
2019-12-06 01:02:31 +0100

    bouttons!

--- view/scripts.js
+++ view/scripts.js
@@ -1,13 +1,19 @@
 function expand() {
 	var liste = Array.from(document.getElementsByClassName("expandButton"));
 	liste.forEach(function(e){
-		e.addEventListener("click", aggrandir(e));
+		e.addEventListener("click", function(){aggrandir(e)});
 	});
 }
 
 function aggrandir(e) {
 	console.log('coucou');
-	e.parentNode.style.height='100%';
+	if (e.parentNode.style.height!='200px') {
+		e.parentNode.style.height = '200px';
+	}
+	else {
+		e.parentNode.style.height='100%';
+	}
+	
 }
 
 window.onload = expand();
--- view/topic/list.php
+++ view/topic/list.php
@@ -3,12 +3,11 @@
 </div>
 <?php
 foreach ($tab_t as $t) {
-	echo '<div class="card"><div class="card-content">';
-	echo '<div>'.htmlspecialchars($t->get("dateTopic")).'</div>';
-	echo '<div>'.htmlspecialchars($t->get("loginCompte")).'</div>';
-	echo '<div class="card-title">'.htmlspecialchars($t->get("nomTopic")).'</div>';
-	echo '<p>'.htmlspecialchars($t->get('texteTopic')).'</p>';
-	echo '<button class="expandButton btn waves-effect waves-light"><i class="material-icons">expand_more</i></button>';
-    
+	echo '<div class="card"><div class="card-content toExpand">';
+		echo '<div>'.htmlspecialchars($t->get("dateTopic")).'</div>';
+		echo '<div>'.htmlspecialchars($t->get("loginCompte")).'</div>';
+		echo '<div class="card-title">'.htmlspecialchars($t->get("nomTopic")).'</div>';
+		echo '<button class="expandButton btn waves-effect waves-light"><i class="material-icons">expand_more</i></button>';   
+		echo '<p>'.htmlspecialchars($t->get('texteTopic')).'</p>';
     echo '</div></div>';
 }
