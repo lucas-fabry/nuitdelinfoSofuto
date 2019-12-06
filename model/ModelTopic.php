71fc09cafdeef89c41205f9e2fa98d80ca99acd6
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-06 06:44:59 +0100

    correction vite fait pour que les topics apparaissent dans leur ordre de répartition

--- model/ModelTopic.php
+++ model/ModelTopic.php
@@ -26,6 +26,25 @@ class ModelTopic extends Model {
         }
     }
 
+    public static function selectAll(){
+        $table_name = "Topic";
+        $class_name = "Model".ucfirst($table_name);
+        try {
+            $rep = Model::$pdo->query("SELECT * FROM $table_name ORDER BY idTopic DESC");
+
+            $rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            return $rep->fetchAll();
+        }
+        catch(PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href="index.php?action=lireTout"> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+
     public static function selectAllByLogin($login) {
         try {
             $sql = "SELECT * from Topic WHERE loginCompte=:l";
