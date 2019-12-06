6c13a98bcc84f78e808c756e523dc447e73186ce
Lucas Fabry <lucas.fabry@outlook.fr>
2019-12-06 02:01:23 +0100

    On me critique mais je sais que je suis solide sur les appuis

--- view/topic/cassetete.php
+++ view/topic/cassetete.php
@@ -1,5 +1,17 @@
-<img src="https://image.aveshack.com/8ce9abc24ddc6457.png"/> <h1>Coffre n°1</h1><p>J'ai la clé</p>
+<div style="position: absolute"><img src="https://image.aveshack.com/8ce9abc24ddc6457.png"/> <p style="margin-left:3vw;">Coffre n°1, j'ai la clé !</p></div>
 
-<img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <h1>Coffre n°2</h1><p>Je n'ai pas la clé</p>
+<div style="position: absolute; margin-left: 35vw;"><img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <p style="margin-left:3vw;">Coffre n°2, j'ai la clé !</p></div>
 
-<img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <h1>Coffre n°3</h1><p>Le premier n'a pas la clé</p>
+<div style="position: absolute; margin-left: 70vw;"><img src="https://image.aveshack.com/8ce9abc24ddc6457.png" /> <p style="margin-left:3vw;">Coffre n°3, j'ai la clé !</p></div>
+
+
+<p style="font-size: 8vh; margin-top: 50vh; text-align: center;">Seul un de ces coffres dit la vérité, où est la clé ?</p>
+
+<div class="container center">
+<div class="row">
+<form class="col s2"method="post" action="cassetete.php">
+    <input class="" type="number" name="valeur"></input>
+    
+</form>
+</div>
+</div>
